8c5a1b5bef7142d6eb0a9290a71311e45ccd31ff
Isidora <110285593+isidoracupara@users.noreply.github.com>
2022-10-19 09:08:11 +0200

    add typing to predict function

--- predict/prediction.py
+++ predict/prediction.py
@@ -1,15 +1,11 @@
-
-    # {
-    # "prediction": Optional[float],
-    # "status_code": Optional[int]
-    # }
-
 import pickle
+from typing import List
 
-def predict(data):
+def predict(model_input: List[int])-> float:
+
     pickle_filename = "pickle_model.pkl"
     with open(pickle_filename,'rb') as file:
         pickle_model = pickle.load(file)
-    y_pred = pickle_model.predict(data)
+    y_pred = pickle_model.predict(model_input)
 
     return y_pred
